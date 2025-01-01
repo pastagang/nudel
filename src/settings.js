438e35fc1ee1fb3a23d56690d823b8e20a004306
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-01-01 18:16:38 +0000

    settings menu that does nothing

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,6 @@
 const settingsButton = document.querySelector("#settings-button");
+const settingsDialog = document.querySelector("#settings-dialog");
+
+settingsButton.addEventListener("click", () => {
+  settingsDialog.showModal();
+});
