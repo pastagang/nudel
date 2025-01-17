325890c0d208b061f7b2b5fb4349ebde2156eb47
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-01-17 17:24:08 +0000

    play button

--- src/menu.js
+++ src/menu.js
@@ -5,58 +5,29 @@ import { changeSettings, getSettings } from './settings.js';
 
 const menuButton = document.querySelector('#menu-button');
 const menuContainer = document.querySelector('#menu-container');
-// const menuContent = document.querySelector('#menu-content');
 
 const aboutButton = document.querySelector('#about-button');
-// const aboutDialogContainer = document.querySelector('#about-dialog-container');
 const helpButton = document.querySelector('#help-button');
 const aboutDialog = document.querySelector('#about-dialog');
-const exportButton = document.querySelector('#export-button');
-const copyAsFlokButton = document.querySelector('#copy-as-flok-button');
-const yesButton = document.querySelector('#about-yes-button');
+const playButton = document.querySelector('#about-yes-button');
 
 menuButton.addEventListener('click', (e) => {
   menuContainer.classList.toggle('open');
 });
 
-// menuContainer.addEventListener(
-//   'blur',
-//   (e) => {
-//     console.log(e);
-//     menuContainer.classList.remove('open');
-//   },
-//   { bubbles: true },
-// );
-
 helpButton?.addEventListener('click', () => {
   nudelAlert('Coming soon');
 });
 
-// welcomeUsernameInput?.addEventListener('input', () => {
-//   const settings = getSettings();
-//   setSettings({
-//     ...settings,
-//     username: welcomeUsernameInput.value,
-//   });
-// });
-
-// yesButton.addEventListener('click', () => {
-//   const settings = getSettings();
-//   if (welcomeUsernameInput?.value) {
-//     setSettings({
-//       ...settings,
-//       username: welcomeUsernameInput.value,
-//     });
-//   }
-// });
+playButton.addEventListener('click', () => {
+  const runButtons = document.querySelectorAll('.run');
+  runButtons.forEach((runButton) => runButton.click());
+});
 
 aboutButton?.addEventListener('click', () => {
   aboutDialog.showModal();
-  yesButton.focus();
+  playButton.focus();
 });
-// panelModeSelectBurger?.addEventListener('change', () => {
-//   updateSettings({ panelMode: panelModeSelectBurger.value });
-// });
 
 const html = document.documentElement;
 
@@ -70,12 +41,10 @@ if (!getSettings().username) {
   const username = await nudelPrompt('hello! what name do you want to go by?');
   changeSettings({ username });
   aboutDialog.showModal();
-  yesButton.focus();
+  playButton.focus();
 } else {
   if (getSettings().welcomeMessage3) {
     aboutDialog.showModal();
-    yesButton.focus();
+    playButton.focus();
   }
 }
-
-// settingsDialog.showModal();
