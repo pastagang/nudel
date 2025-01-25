f7f6d07f1dd9d7b09b100a828150e80566635f94
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-01-25 16:42:43 +0000

    speechda

--- src/std.js
+++ src/std.js
@@ -1,5 +1,31 @@
-window.spagda = function spagda(name) {
+function spag(name) {
+  return `https://spag.cc/${name}`;
+}
+
+function spagda(name) {
   samples({
-    [name]: `https://spag.cc/${name}`,
+    [name]: spag(name),
   });
-};
+}
+
+function speechda(
+  wordsArg = '',
+  // default to PC music
+  locale = 'en-GB',
+  gender = 'f',
+) {
+  const words = wordsArg
+    .replaceAll(' ', ',')
+    .split(',')
+    .map((word) => word.trim())
+    .filter((word) => word.length > 0);
+
+  if (words.length === 0) {
+    return;
+  }
+  samples(`shabda/speech/${locale}/${gender}:${words.join(',')}`);
+}
+
+window.speechda = speechda;
+window.spagda = spagda;
+window.spag = spag;
