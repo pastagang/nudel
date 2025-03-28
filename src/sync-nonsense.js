708d67f6f213431a0f29dbc7ed2e77137003b5ab
Felix Roos <flix91@gmail.com>
2025-03-28 21:24:50 +0100

    more sync nonsense

--- src/sync-nonsense.js
+++ src/sync-nonsense.js
@@ -1,4 +1,3 @@
-import { TimeSpan } from '@strudel/core';
 import { getSecondsSinceNudelDayStart } from './weather.js';
 
 let secondsSinceNudelDayStartCache = null;
@@ -7,9 +6,15 @@ let syncOrigin;
 
 export function initSync(ctxTime) {
   syncOrigin = getSecondsSinceNudelDayStart() / 1000 + ctxTime;
+  globalThis.syncOrigin = syncOrigin;
   console.log('syncOrigin', syncOrigin);
 }
 globalThis.initSync = initSync;
+
+export function getSyncOrigin() {
+  return syncOrigin;
+}
+globalThis.getSyncOrigin = getSyncOrigin;
 
 export function getSyncOffset() {
   if (secondsSinceNudelDayStartCache === null) {
