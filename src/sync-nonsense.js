aa9d27ec9c3ba4a3241606f6c3c4882cd5963e66
Felix Roos <flix91@gmail.com>
2025-03-28 21:16:47 +0100

    try setting sync origin after audio init

--- src/sync-nonsense.js
+++ src/sync-nonsense.js
@@ -2,10 +2,16 @@ import { TimeSpan } from '@strudel/core';
 import { getSecondsSinceNudelDayStart } from './weather.js';
 
 let secondsSinceNudelDayStartCache = null;
-export function getSyncOffset() {
-  // For debugging
-  return 100000;
 
+let syncOrigin;
+
+export function initSync(ctxTime) {
+  syncOrigin = getSecondsSinceNudelDayStart() / 1000 + ctxTime;
+  console.log('syncOrigin', syncOrigin);
+}
+globalThis.initSync = initSync;
+
+export function getSyncOffset() {
   if (secondsSinceNudelDayStartCache === null) {
     secondsSinceNudelDayStartCache = getSecondsSinceNudelDayStart();
     console.log('SYNC OFFSET:', secondsSinceNudelDayStartCache);
