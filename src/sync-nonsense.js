cb8a583747f1c47cf1fa5955d787024cbaa8fad5
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-03-28 19:19:44 +0000

    comments

--- src/sync-nonsense.js
+++ src/sync-nonsense.js
@@ -3,8 +3,9 @@ import { getSecondsSinceNudelDayStart } from './weather.js';
 
 let secondsSinceNudelDayStartCache = null;
 export function getSyncOffset() {
-  // 10 for now for debugging
+  // For debugging
   // return 1;
+
   if (secondsSinceNudelDayStartCache === null) {
     secondsSinceNudelDayStartCache = getSecondsSinceNudelDayStart();
     console.log('SYNC OFFSET:', secondsSinceNudelDayStartCache);
