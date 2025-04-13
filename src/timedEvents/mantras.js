a8bd6070c55fcc7c79a08f768769e23e1c75ef6d
Lu[ke] Wilson <l2wilson94@gmail.com>
2025-04-14 00:32:16 +0100

    go

--- src/timedEvents/mantras.js
+++ src/timedEvents/mantras.js
@@ -1,55 +1,15 @@
 import { getWeather } from './climate.js';
 
-export const MANTRAS = [
-  // cherry picking some mantras for the algorave tonight
-
-  'upload photos and videos to spag.cc',
-  //'no stars',
-  //'we love repetition',
-  //'there is only one mantra',
-  'BAD CODE ONLY',
-  'energy YES. quality NO.',
-  // 'be brave',
-  'normalise sharing scrappy fiddles',
-  //'embrace death',
-  'let code die',
-  // 'let go of what you know',
-  'move',
-  'make space',
-  'make room for others',
-  'you must delete',
-  // 'delete a mantra',
-  // 'add a conditional mantra',
-  // 'embrace spaghetti code',
-  // 'all day every day',
-  // "we'll never release, but we already have",
-  'test in the room',
-  'ship a feature NOW',
-  //'question mantras',
-  //'you are not dead yet',
-  // 'connect',
-  // 'mantras are code',
-  'climate is what determines the weather',
-];
+export const MANTRAS = ['make another flok client'];
 
 const CONDITIONAL_MANTRAS = [
-  {
-    condition: () => !getWeather().clearSkies,
-    mantras: [
-      //'maybe you like the weather', 'BAD WEATHER ONLY'
-    ],
-  },
   {
     condition: () => getWeather().mantraName,
     mantras: ['pastagang'],
   },
   {
-    condition: () => getWeather().grayScale,
-    mantras: [],
-  },
-  {
     condition: () => getWeather().invertedColors,
-    mantras: ['colors * -1, energy * 10', 'ʎluo ǝpoɔ pɐq'],
+    mantras: ['ʎluo ǝpoɔ pɐq'],
   },
   {
     condition: () => getWeather().noNudel,
@@ -63,15 +23,7 @@ const CONDITIONAL_MANTRAS = [
   },
   {
     condition: () => getWeather().kaleidoscope,
-    mantras: ['thou shalt use kaleidescope'],
-  },
-  {
-    condition: () => getWeather().pixelated,
-    mantras: ['pixels are an amazing way to make art'],
-  },
-  {
-    condition: () => getWeather().palindromeNames,
-    mantras: [],
+    mantras: ['thou shalt not use kaleidescope'],
   },
 ];
 
