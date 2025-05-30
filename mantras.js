9f6f93b442299a8eaf6b16d0e78a0852b5ce6d07
Lu Wilson <l2wilson94@gmail.com>
2025-05-30 18:39:03 +0200

    Update mantras.js

--- mantras.js
+++ mantras.js
@@ -1,6 +1,11 @@
 import { getWeather } from './climate.js';
 
-export const MANTRAS = ['do it', 'anything can happen'];
+export const MANTRAS = [
+  'do it', 
+  'anything can happen',
+    'shave your head. its pastagang time',
+  'forget everything you know'
+];
 
 export const OLD_MANTRAS = [
   'mantras repeat',
